526b2802edb5f9eba9ff8590d267b6809ac9f718
therayyan2 <therayyanahmd@gmail.com>
2025-03-25 14:43:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ st.write("AI-powered image generation at its best! 🚀")
 
 c1,c2,c3 = st.columns(3)
 with c1:
-    st.image("image1.webp")
+    st.image("static/image1.webp")
 with c2:
-    st.image("image2.webp")
+    st.image("static/image2.webp")
 with c3:
-    st.image("image3.webp")
+    st.image("static/image3.webp")
 c4,c6 = st.columns(2)
 with c4:
-    st.image("image4.webp")
+    st.image("static/image4.webp")
 with c6:
-    st.image("image6.webp")
+    st.image("static/image6.webp")
 c7,c8,c9 = st.columns(3)
 with c7:
-    st.image("image7.webp")
+    st.image("static/image7.webp")
 with c8:
-    st.image("image8.webp")
+    st.image("static/image8.webp")
 with c9:
-    st.image("image9.webp")
+    st.image("static/image9.webp")
